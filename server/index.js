d88688fbc5026df419778daf03a9ec967779d565
NickZavadski <nick.mr.volk@gmail.com>
2023-09-05 18:51:33 +0200

    add first request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
 
-const PORT = process.env.PORT || e000
+const PORT = process.env.PORT || 3000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.status(200).json({ message: 'WORKING!!!' })
+})
 
 const start = async () => {
 	try {
