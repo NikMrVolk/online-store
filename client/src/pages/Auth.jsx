dcf81cb229e270196d2f9ac30e2a93a5babee779
NickZavadski <nick.mr.volk@gmail.com>
2023-09-17 21:03:36 +0200

    add login and autorization page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,5 +1,40 @@
-const Auth = ({ props }) => {
-	return <div>AUTH</div>
+import Container from 'react-bootstrap/esm/Container'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/esm/Button'
+import { Link, useLocation } from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
+
+const Auth = () => {
+	const location = useLocation()
+	const isLogin = location.pathname === LOGIN_ROUTE
+
+	return (
+		<Container
+			className="d-flex justify-content-center align-items-center"
+			style={{ height: window.innerHeight - 54 }}
+		>
+			<Card style={{ width: 600 }} className="p-5">
+				<h2 className="m-auto">{isLogin ? 'Autorization' : 'Registration'}</h2>
+				<Form className="d-flex flex-column">
+					<Form.Control placeholder="Enter email..." className="mt-3" type="text" />
+					<Form.Control placeholder="Enter password..." className="mt-3" type="password" />
+					<div className="d-flex justify-content-between align-items-center mt-3">
+						{isLogin ? (
+							<div>
+								Don't have accaunt? <Link to={REGISTRATION_ROUTE}>Sign up</Link>
+							</div>
+						) : (
+							<div>
+								Do you have accaunt? <Link to={LOGIN_ROUTE}>Sign in</Link>
+							</div>
+						)}
+						<Button variant={'outline-success'}>{isLogin ? 'Sign in' : 'Sign up'}</Button>
+					</div>
+				</Form>
+			</Card>
+		</Container>
+	)
 }
 
 export default Auth
